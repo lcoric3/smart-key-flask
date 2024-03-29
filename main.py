bdb405e7b0604592850c38e1af3a6b51430bb910
lcoric3 <luka.coric3@gmail.com>
2024-03-29 22:18:32 +0100

    Summary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length
+from datetime import datetime as dt
 
 from services.user_services import UserServices
 from models.users import User
@@ -12,9 +14,9 @@ app.config['SECRET_KEY'] = 'yekuc60n6T2lgumg7y40mIXhGQw9YKCul2f6OuTDm51e09dGNO5s
 
 
 class UserCreateForm(FlaskForm):
-    first_name = StringField('Ime')
-    last_name = StringField('Prezime')
-    pin_code = StringField('PIN')
+    first_name = StringField('Ime', validators=[DataRequired('Ovo polje je obvezno'), Length(max=50, message='Maksimalno 50 znakova')])
+    last_name = StringField('Prezime', validators=[DataRequired('Ovo polje je obvezno'), Length(max=100, message='Maksimalno 100 znakova')])
+    pin_code = StringField('PIN', validators=[DataRequired('Ovo polje je obvezno'), Length(min=4, max=4, message='Mora biti tsno 4 znaka')])
     is_active = BooleanField('Aktivan?')
     submit = SubmitField('Snimi')
 
@@ -23,9 +25,10 @@ class UserCreateForm(FlaskForm):
 # http://www.domena.hr/
 @app.route('/')
 def index():
-    user = UserServices().get_user()
+    users = UserServices().get_users()
+    current_date = dt.now().strftime('%d.%m.%Y. %H:%M:%S')
     
-    return render_template('index.html', user=user)
+    return render_template('index.html', users=users, time=current_date)
 
 
 # http://www.domena.hr/about
@@ -39,17 +42,42 @@ def about():
 def create_user():
     user_form = UserCreateForm()
 
-    if user_form.validate_on_submit():
+    if user_form.is_submitted():
         user_first_name = user_form.first_name.data
         user_last_name = user_form.last_name.data
         user_pin_code = user_form.pin_code.data
         user_is_active = user_form.is_active.data
+        print(user_first_name, user_last_name, user_pin_code, user_is_active)
         UserServices.create_user(User(user_first_name, user_last_name, user_pin_code, user_is_active))
-        redirect(url_for('index'))
+        return redirect(url_for('index'))
 
     return render_template('create-user.html', form=user_form)
 
 
 
+# http://www.domena.hr/edit-user/2
+@app.route('/edit-user/<id>', methods=['GET', 'POST'])
+def edit_user(id):
+    user_form = UserCreateForm()
+
+    if request.method == 'GET':
+        user = UserServices.get_user(id)
+        user_form.first_name.data = user.first_name
+        user_form.last_name.data = user.last_name
+        user_form.pin_code.data = user.pin_code
+        user_form.is_active.data = user.is_active
+
+    if user_form.is_submitted():
+        user_first_name = user_form.first_name.data
+        user_last_name = user_form.last_name.data
+        user_pin_code = user_form.pin_code.data
+        user_is_active = user_form.is_active.data
+        print(user_first_name, user_last_name, user_pin_code, user_is_active)
+        UserServices.create_user(User(user_first_name, user_last_name, user_pin_code, user_is_active))
+        return redirect(url_for('index'))
+
+    return render_template('edit-user.html', form=user_form)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8888)
